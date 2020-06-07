18d34fe3f40d010646c06b753653a12a31b6a50b
Paweł Mikłas <pablomiklas@gmail.com>
2020-06-08 00:26:17 +0200

    added checking token and route on app mount

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory, Redirect } from "react-router-dom";
+
+import ProtectedRoute from "misc/ProtectedRoute";
+import { getToken } from "utils/authentication";
 
 import Login from "pages/Unauthorized/Login";
 import Home from "pages/Authorized/Home";
 import ErrorPage from "pages/Error";
 
-import ProtectedRoute from "misc/ProtectedRoute";
-
 import useStyle from "./App.styles";
 
 const App = () => {
   const classes = useStyle();
+  const history = useHistory();
+
+  useEffect(() => {
+    const currentLocation = history.location.pathname;
+    const isOnUnauthorizedRoute = currentLocation === "/login" || currentLocation === "/";
+
+    const token = getToken();
+    if (token && isOnUnauthorizedRoute) {
+      history.push("/home");
+    }
+  }, []);
 
   return (
     <div className={classes.wrapper}>
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <ProtectedRoute exact path="/home" component={Home} />
-          <Route exact path="/" render={() => <Redirect to="/login" />} />
-          <Route path="*" component={ErrorPage} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/home" component={Home} />
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+        <Route path="*" component={ErrorPage} />
+      </Switch>
     </div>
   );
 };
