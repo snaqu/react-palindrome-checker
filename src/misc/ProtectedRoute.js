b6093aa7ffbd0ae09a945964e839728e89ca426b
Paweł Mikłas <pablomiklas@gmail.com>
2020-06-07 19:33:24 +0200

    handled isAuthenticated util in protected route

--- src/misc/ProtectedRoute.js
+++ src/misc/ProtectedRoute.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
+
+import { isAuthenticated } from "utils/authentication";
 
 const ProtectedRoute = ({ ...props }) => {
-  const { isAuthenticated } = useSelector(({ user }) => user);
-
-  return isAuthenticated ? <Route {...props} /> : <Redirect to="/" />;
+  return isAuthenticated() ? <Route {...props} /> : <Redirect to="/" />;
 };
 
 export default ProtectedRoute;
