3b9595fb0fdc929bde1508bea1018ac671b3ee24
Paweł Mikłas <pablomiklas@gmail.com>
2020-06-07 19:32:33 +0200

    added redirect in app file if user has token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
 
 import ProtectedRoute from "misc/ProtectedRoute";
+import { getToken } from "utils/authentication";
 
 import Login from "pages/Unauthorized/Login";
 import Home from "pages/Authorized/Home";
@@ -10,6 +11,15 @@ import useStyle from "./App.styles";
 
 const App = () => {
   const classes = useStyle();
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = getToken();
+
+    if (token) {
+      history.push("/home");
+    }
+  }, []);
 
   return (
     <div className={classes.wrapper}>
